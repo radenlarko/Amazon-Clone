2bdec869a98725cd4d63e75d0c1e156f639afbe6
radenlarko <radenlarko@gmail.com>
2022-06-13 17:11:06 +0700

    Setup login google next auth

--- components/Header.jsx
+++ components/Header.jsx
@@ -4,6 +4,7 @@ import {
   SearchIcon,
   ShoppingCartIcon,
 } from "@heroicons/react/outline";
+import {signIn, signOut, useSession} from "next-auth/react"
 
 const Header = () => {
   return (
@@ -27,7 +28,7 @@ const Header = () => {
           <SearchIcon className="h-12 p-4" />
         </div>
         <div className="text-white flex items-center text-xs space-x-6 mx-6 whitespace-nowrap">
-          <div className="link">
+          <div onClick={signIn} className="link">
             <p>Hello Yos Sularko</p>
             <p className="font-extrabold md:text-sm">Account & Lists</p>
           </div>
---
+++ firebase.js
@@ -0,0 +1,10 @@
+import firebase from "firebase";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyC1CJ9O2kBebQDqlU60-m2JRbXsEPBxXAs",
+  authDomain: "amzn-projt.firebaseapp.com",
+  projectId: "amzn-projt",
+  storageBucket: "amzn-projt.appspot.com",
+  messagingSenderId: "943746888928",
+  appId: "1:943746888928:web:748c19c53c12a5ff7a8c85",
+};
