e2dd189f7e94c3c922a975662372ee4ce285ddb3
radenlarko <radenlarko@gmail.com>
2022-06-15 17:18:55 +0700

    setup checkout page setup redux add to basket

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,13 +1,18 @@
 import Image from "next/image";
+import { useRouter } from "next/router";
 import {
   MenuIcon,
   SearchIcon,
   ShoppingCartIcon,
 } from "@heroicons/react/outline";
 import { signIn, signOut, useSession } from "next-auth/react";
+import { useSelector } from "react-redux";
+import { selectItems } from "../slices/basketSlice";
 
 const Header = () => {
   const { data: session } = useSession();
+  const router = useRouter();
+  const items = useSelector(selectItems)
   return (
     <header>
       <div className="flex items-center bg-amazon_blue p-1 flex-grow py-2">
@@ -19,6 +24,7 @@ const Header = () => {
             objectFit="contain"
             className="cursor-pointer"
             alt="logo-amazon"
+            onClick={() => router.push("/")}
           />
         </div>
         <div className="hidden sm:flex items-center h-10 rounded-md flex-grow cursor-pointer bg-yellow-400 hover:bg-yellow-500">
@@ -37,9 +43,12 @@ const Header = () => {
             <p>Returns</p>
             <p className="font-extrabold md:text-sm">& Orders</p>
           </div>
-          <div className="relative link flex items-center">
+          <div
+            onClick={() => router.push("/checkout")}
+            className="relative link flex items-center"
+          >
             <span className="absolute top-0 right-0 md:right-10 h-4 w-4 bg-yellow-400 text-center rounded-full text-black font-bold">
-              0
+              {items.length}
             </span>
             <ShoppingCartIcon className="h-10" />
             <p className="hidden md:inline font-extrabold md:text-sm mt-2">
--- components/Product.jsx
+++ components/Product.jsx
@@ -2,8 +2,11 @@ import Image from "next/image";
 import { useState, useEffect } from "react";
 import { StarIcon } from "@heroicons/react/solid";
 import CurrencyFormat from "react-currency-format";
+import { useDispatch } from "react-redux";
+import { addToBasket } from "../slices/basketSlice";
 
 const Product = ({ id, title, price, description, category, image }) => {
+  const dispatch = useDispatch();
   const [rating, setRating] = useState(1);
   const [hasPrime, setHasPrime] = useState(false);
 
@@ -20,9 +23,17 @@ const Product = ({ id, title, price, description, category, image }) => {
     setHasPrime(prime);
   }, []);
 
+  const addItemToBasket = () => {
+    const product = { id, title, price, description, category, image };
+
+    dispatch(addToBasket(product));
+  };
+
   return (
     <div className="relative flex flex-col m-5 bg-white z-30 p-10">
-      <p className="absolute top-2 right-2 text-xs italic text-gray-400">{category}</p>
+      <p className="absolute top-2 right-2 text-xs italic text-gray-400">
+        {category}
+      </p>
       <Image
         src={image}
         width={200}
@@ -54,7 +65,9 @@ const Product = ({ id, title, price, description, category, image }) => {
         </div>
       )}
 
-      <button className="mt-auto button">Add to Basket</button>
+      <button onClick={addItemToBasket} className="mt-auto button">
+        Add to Basket
+      </button>
     </div>
   );
 };
